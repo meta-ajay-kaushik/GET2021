9ce67cb6bed23956587715b2795ac1fc20d66caa
Ajay Kaushik <ajay.kaushik@metacube.com>
2021-03-10 14:26:01 +0530

    updated shopping cart

--- Software Construction Fundamental Overview Assignment/ShoppingCart.java
+++ Software Construction Fundamental Overview Assignment/ShoppingCart.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 import java.util.HashMap;
 // Shopping cart main class
-public class shopping_cart {
+public class ShoppingCart {
 	// initialize global variables of items, price and value	
-    static String items[] = { "Bat", "Ball", "Badminton", "Bottle" };
-    static int price[] = { 1000, 50, 250, 100 };
-    static int value[] = { 10, 20, 15, 25 };
-    static Scanner scan = new Scanner(System.in);
+    String items[];
+    int price[];
+    int stocks[];
+    public ShoppingCart(String[] items,int[] price, int[] stocks) {
+		this.items=items;
+		this.price=price;
+		this.stocks=stocks;
+	}
+    Scanner scan = new Scanner(System.in);
     // create Hash map for user cart
-    static HashMap<String, Integer> cart = new HashMap<String, Integer>();
+    HashMap<String, Integer> cart = new HashMap<String, Integer>();
     // method to display item menu of shop    
-    static void display_items(String[] items, int[] value, int[] price) {
+    void display_items(String[] items, int[] value, int[] price) {
         System.out.println("Available items in shop - ");
         System.out.println("S.no Item Value Price");
         for (int i = 0; i < items.length; i++) {
@@ -18,21 +23,21 @@ public class shopping_cart {
         }
     }
     // method to display cart items
-    static void display_cart(HashMap<String, Integer> cart) {
+    void display_cart(HashMap<String, Integer> cart) {
         System.out.println(cart);
-        display_items(items, value, price);
+        display_items(items, stocks, price);
         operations();
     }
     // method to add items in user cart
-    static void add_to_cart(HashMap<String, Integer> cart, int[] value) {
+    void add_to_cart(HashMap<String, Integer> cart, int[] stocks) {
         System.out.println("Enter S.no of item which you want to add to your cart");
         int item_number = scan.nextInt();
-        if (item_number <= value.length && item_number > 0) {
+        if (item_number <= stocks.length && item_number > 0) {
             System.out.println("Enter value of item you want to you");
             int val_item = scan.nextInt();
-            if (val_item <= value[item_number - 1]) {
+            if (val_item <= stocks[item_number - 1]) {
                 System.out.println("Item added to cart...");
-                value[item_number - 1] -= val_item;
+                stocks[item_number - 1] -= val_item;
                 // cart.put(items[item_number - 1], val_item);
                 if (cart.get(items[item_number - 1]) != null) {
                     cart.put(items[item_number - 1], cart.get(items[item_number - 1]) + val_item);
@@ -42,24 +47,25 @@ public class shopping_cart {
                 display_cart(cart);
             } else {
                 System.out.println("value is greater than available - try again");
-                add_to_cart(cart, value);
+                add_to_cart(cart, stocks);
             }
         } else {
             System.out.println("wrong item number, try again");
-            add_to_cart(cart, value);
+            add_to_cart(cart, stocks);
         }
     }
     // method to remove items from cart
-    static void remove_from_cart(HashMap<String, Integer> cart, int[] value) {
+    void remove_from_cart(HashMap<String, Integer> cart, int[] stocks) {
         System.out.println("Enter S.no of item which you want to remove from your cart");
         int item_number = scan.nextInt();
-        if (item_number <= value.length && item_number > 0) {
+        if (item_number <= stocks.length && item_number > 0) {
             if (cart.get(items[item_number - 1]) != null) {
                 System.out.println("Enter value of item you want to remove");
                 int val_item = scan.nextInt();
                 if (val_item < cart.get(items[item_number - 1])) {
                     System.out.println("Item updated to cart...");
                     cart.put(items[item_number - 1], cart.get(items[item_number - 1]) - val_item);
+                    stocks[item_number - 1] += val_item;
                     display_cart(cart);
                 } else {
                     cart.remove(items[item_number - 1]);
@@ -75,7 +81,7 @@ public class shopping_cart {
         }
     }
     // method to generate user bill
-    static void generate_bill(HashMap<String, Integer> cart) {
+    void generate_bill(HashMap<String, Integer> cart) {
         int total_bill = 0;
         if (cart.isEmpty()) {
             System.out.println("Your cart is empty");
@@ -91,7 +97,7 @@ public class shopping_cart {
         operations();
     }
     // method of all operations of shop
-    static void operations() {
+    void operations() {
         System.out.println(
                 "\nEnter 1 to view your cart\nEnter 2 to add item to your cart\nEnter 3 to remove item from your cart\nEnter 4 to generate bill\nEnter 5 to exit from shop");
         int input = scan.nextInt();
@@ -100,10 +106,10 @@ public class shopping_cart {
                 display_cart(cart);
                 break;
             case 2:
-                add_to_cart(cart, value);
+                add_to_cart(cart, stocks);
                 break;
             case 3:
-                remove_from_cart(cart, value);
+                remove_from_cart(cart, stocks);
                 break;
             case 4:
                 generate_bill(cart);
@@ -118,8 +124,12 @@ public class shopping_cart {
     }
     // main method of class shopping cart
     public static void main(String[] args) {
-        display_items(items, value, price);
-        operations();
-        scan.close();
+    	String items[] = { "Bat","Video Game", "Ball", "Badminton", "Bottle" };
+        int price[] = { 1000,2400, 50, 250, 100 };
+        int stocks[] = { 10,15, 20, 15, 25 };
+    	ShoppingCart s=new ShoppingCart(items, price, stocks);
+        s.display_items(s.items, s.stocks, s.price);
+        s.operations();
+        s.scan.close();
     }
 }
