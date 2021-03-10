95357e39691221384cc06b740b55afdaf176307b
Ajay Kaushik <ajay.kaushik@metacube.com>
2021-03-10 11:18:15 +0530

    update

--- DSA_4/molecularmass/MolecularMass.java
+++ DSA_4/molecularmass/MolecularMass.java
@@ -1,122 +1,76 @@
 package molecularmass;
+import java.util.*;
 // Main Java class MolecularMass
 public class MolecularMass {
-	// method to get atomic weight of atoms
-	public int getAtomicWeight(Character atom)
-	{
-		int atomicWeight=-1;
-		if(atom=='C')
-		{
-			atomicWeight=12;
-		}
-		else if(atom=='H')
-		{
-			atomicWeight=1;
-		}
-		else if(atom=='O')
-		{
-			atomicWeight=16;
-		}
-		else if(atom=='(')
-		{
-			atomicWeight=-2;
-		}
-		return atomicWeight;
+	Map<Character, Integer> elements = new HashMap<Character, Integer>();
+	// constructor
+	public MolecularMass() {
+		elements.put('H', 1);
+		elements.put('C', 12);
+		elements.put('O', 16);
 	}
-	// method to find multiplier
-	public int[] findMultiplier(String compound,int index)
-	{
-		String str=""+compound.charAt(index);
-		int arr[]=new int[2];
-		arr[0]=1;
-		for(int i=index+1;i<compound.length();i++)
-		{
-			if(this.getAtomicWeight(compound.charAt(i))==-1)
-			{
-				arr[0]=arr[0]+1;
-				str=str+compound.charAt(i);
+	// method to calculate molecular mass
+	public int calculateMolecularMass(String molecule) {
+		int massOfMolecule = 0;
+		String block = "";
+		
+		for (int index = 0; index < molecule.length(); index++) {
+			char character = molecule.charAt(index);
+			
+			if (character != '(') {
+				if (character == 'H' || character == 'C' || character == 'O') {
+					if (molecule.charAt(index + 1) >= 50 && molecule.charAt(index + 1) <= 57) {
+						massOfMolecule = massOfMolecule + (elements.get(character) * (molecule.charAt(index + 1) - 48));
+						index++;
+					} else {
+						massOfMolecule = massOfMolecule + elements.get(character);
+					}
+				}
 			}
-			else
-			{
-				break;
+			
+			else if (character == '(') {
+				while (molecule.charAt(index) != ')' && index < molecule.length()) {
+					index++;
+					block = block + molecule.charAt(index);
+				} 
+				index++;
+				if (molecule.charAt(index) >= 50 && molecule.charAt(index) <= 57) {
+					massOfMolecule = massOfMolecule + (calculateMassOfBlock(block) * (molecule.charAt(index) - 48));
+					index++;
+				} else {
+					massOfMolecule = massOfMolecule + calculateMassOfBlock(block);
+				}
 			}
 		}
-		arr[1]=Integer.parseInt(str);
-		return arr;
+		
+		return massOfMolecule;
 	}
-	// method to calculate molecular weight of the compound
-	public int getMolecularWeight(String compound)
-	{
-		int totalWeight=0;
-		for(int i=0;i<compound.length();i++)
-		{
-			int atomicWeight=this.getAtomicWeight(compound.charAt(i));
-			int multiplier=1;
-			if(atomicWeight==-2)
-			{
-				int find=-1;
-				for(int j=i+1;j<compound.length();j++)
-				{
-					if(compound.charAt(j)==')')
-					{
-						find=j;
-						break;
-					}
-				}
-				if(find==-1)
-				{
-					return find;
-				}
-				else
-				{
-					int wght=this.getMolecularWeight(compound.substring(i+1,find));
-					i=find;
-					if(i<compound.length())
-					{
-					if(this.getAtomicWeight(compound.charAt(find+1))==-1)
-					{
-						wght=wght*Integer.parseInt(Character.toString(compound.charAt(find+1)));
-						i=i+1;
-					}
-					}
-					totalWeight=totalWeight+wght;
+	// method to calculate molecular mass of a block
+	private int calculateMassOfBlock(String block) {
+		int sumOfBlock = 0;
+		
+		for (int index = 0; index < block.length(); index++) {
+			
+			if(block.charAt(index) == 'H' || block.charAt(index) == 'C' || block.charAt(index) == 'O') {
+			
+				if (block.charAt(index + 1) >= 50 && block.charAt(index + 1) <= 57) {
+					sumOfBlock = sumOfBlock + elements.get(block.charAt(index)) * (block.charAt(index + 1) - 48);
+				} else {
+					sumOfBlock = sumOfBlock + elements.get(block.charAt(index));
 				}
 			}
-			else if(atomicWeight!=-1)
-			{
-				if(i!=compound.length()-1)
-				{
-					int nextatomicWeight=this.getAtomicWeight(compound.charAt(i+1));
-					if(nextatomicWeight==-1)
-					{
-						int arr[]=this.findMultiplier(compound,i+1);
-						multiplier=arr[1];
-						i=i+arr[0];
-					}
-				}
-			}
-			 if(atomicWeight==1)
-			{
-				totalWeight=totalWeight+multiplier;
-			}
-			else if(atomicWeight==12)
-			{
-				totalWeight=totalWeight+multiplier*12;
-			}
-			else if(atomicWeight==16)
-			{
-				totalWeight=totalWeight+multiplier*16;
-			}		
 		}
-		return totalWeight;
+		return sumOfBlock;
 	}
 	// main method
 	public static void main(String []args)
 	{
 		MolecularMass molecularWeight=new MolecularMass();
-//		String compound="CH3(CH3)2H2O";
-		String compound="C(OH)2";
-		int weight=molecularWeight.getMolecularWeight(compound);
+		String compound="CO2(CH3)2";
+//		String compound="C2H2O4";
+//		String compound="(COOH)2";
+//		String compound="C(OH)2";
+		int weight=molecularWeight.calculateMolecularMass(compound);
 		System.out.println("weight of "+compound +" is = "+weight);
 	}
 }
